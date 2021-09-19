fc68bd7202306e3782a596e2d72eb84be3f7d78f
Yeeeeee1 <KostyaEK2006@gmail.com>
2021-09-19 20:40:31 +0300

    feat: add styles for header

--- rs-shop/src/app/app.module.ts
+++ rs-shop/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/components/header/header/header.component';
-import { FooterComponent } from './core/components/footer/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SliderComponent } from './shop/components/main-page/slider/slider.component';
--- rs-shop/src/app/core/components/header/header.component.ts
+++ rs-shop/src/app/core/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
 import { IpService } from 'src/app/core/services/ip.service';
+import { CatalogService } from 'src/app/shop/services/catalog.service';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +11,65 @@ import { IpService } from 'src/app/core/services/ip.service';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   city = '';
-  constructor(private ipService: IpService) {}
+  showModal = false;
+  name: string | null = '';
+  login = '';
+  password = '';
+  data: any = [];
+  constructor(
+    private ipService: IpService,
+    private modalService: NgbModal,
+    private catalogService: CatalogService
+  ) {}
 
   ipSub: Subscription | null = new Subscription();
 
+  openSm(content: any): void {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  auth(): void {
+    if (this.login && this.password) {
+      localStorage.setItem('login', this.login);
+      localStorage.setItem('password', this.password);
+      this.name = this.login;
+      this.modalService.dismissAll();
+    }
+  }
+
+  signOutFunc(): void {
+    localStorage.removeItem('login');
+    localStorage.removeItem('password');
+    this.name = '';
+  }
+
+  changeCity(city: string): void {
+    this.city = city;
+  }
+
+  search(e: Event): void {
+    alert('Не работает!');
+    /* if ((<HTMLInputElement>e.target).value.length > 2) {
+      this.catalogService.getCatalogProducts().subscribe((data: any) => {
+        console.log(data);
+        data.map((value: any) => {
+          console.log(value.name);
+          value.subCategories.map((value: any) =>
+            value.name
+              .toLowerCase()
+              .includes((<HTMLInputElement>e.target).value) === true
+              ? this.data.push({ id: value.id, name: value.name })
+              : null
+          );
+        });
+        console.log(this.data);
+      });
+    } */
+  }
+
   ngOnInit(): void {
+    this.name = localStorage.getItem('login');
     this.ipService.getApi().subscribe((data) => {
-      console.log(data);
       this.city = data.city;
     });
   }
